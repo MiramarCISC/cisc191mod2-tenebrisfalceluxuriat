aae1fd8df747d62fa87e64101c1ed7a442240c81
Blace_magician <mcalisterlake@gmail.com>
2026-04-17 12:46:49 -0700

    sure

--- src/main/java/edu/sdccd/cisc191/Main.java
+++ src/main/java/edu/sdccd/cisc191/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO create students
-
+        Student[] students = new Student[4];
         // TODO demonstrate filtering
 
         // TODO demonstrate sorting
